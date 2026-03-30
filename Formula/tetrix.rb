28e1922bcc7f333ab1831f216ba8ba73a5471a60
Wellington Prado <wellington@deskree.com>
2026-03-30 17:13:33 -0300

    Update tetrix formula to v0.1.1

--- Formula/tetrix.rb
+++ Formula/tetrix.rb
@@ -1,28 +1,28 @@
 class Tetrix < Formula
   desc "CLI installer for Tetrix CE — AI-powered code understanding platform"
   homepage "https://github.com/well-prado/tetrix-cli"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/well-prado/tetrix-cli/releases/download/v0.1.0/tetrix_darwin_arm64.tar.gz"
-      sha256 "64b193c7618d266519f3d3470f536de05f3add1ddf1c86827ec3985bffd4dfc1"
+      url "https://github.com/well-prado/tetrix-cli/releases/download/v0.1.1/tetrix_darwin_arm64.tar.gz"
+      sha256 "610c6d8bbd81faa3b3930b545c26d862aa8842f748910347cfef9e15ca92d9de"
     end
     on_intel do
-      url "https://github.com/well-prado/tetrix-cli/releases/download/v0.1.0/tetrix_darwin_amd64.tar.gz"
-      sha256 "333da82455a51157afc750745fad078e3325784b3259335d1eeca5ef5fd846a3"
+      url "https://github.com/well-prado/tetrix-cli/releases/download/v0.1.1/tetrix_darwin_amd64.tar.gz"
+      sha256 "60e9bb019eb8608536cfa06372aab9fd64ae4e1729c0fd86a35d92af6487c58b"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/well-prado/tetrix-cli/releases/download/v0.1.0/tetrix_linux_arm64.tar.gz"
-      sha256 "0e1d943908a717b17bfdf3d52290c3815db35bd11246c645e849425023cd31f2"
+      url "https://github.com/well-prado/tetrix-cli/releases/download/v0.1.1/tetrix_linux_arm64.tar.gz"
+      sha256 "0fa291f813ed9a652ed003ec89c73b94bb83214f3f26203e9c90de4ba74e6d1a"
     end
     on_intel do
-      url "https://github.com/well-prado/tetrix-cli/releases/download/v0.1.0/tetrix_linux_amd64.tar.gz"
-      sha256 "ff11844318f91a537ef2556e88ed52e9f394a170888be7506bc1641f6cb48f37"
+      url "https://github.com/well-prado/tetrix-cli/releases/download/v0.1.1/tetrix_linux_amd64.tar.gz"
+      sha256 "852e750ac4f19345f38e35402f0b5a7b0b694f5896346217466a2097d9967e0c"
     end
   end
 
